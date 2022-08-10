2b4318f01a953cdcff85fc4bfa42d273ae608444
shenguoqing <yinweiyingzhe@126.com>
2022-08-10 14:45:47 +0800

    beta8

--- src/data/string/findTargetInStr.ts
+++ src/data/string/findTargetInStr.ts
@@ -4,16 +4,16 @@ import exportSelfMsg from './../utils/exportSelfMsg'
  * 获取指定字符串在目标字符串中的位置数组,理论上不单独调用
  * @param {string} str 目标字符串
  * @param {string} target 需要查找的字符串
- * @param {false | number} limitNum 限制数量,false不限制
+ * @param {number} limitNum 限制数量,false不限制
  * @param {number[]} list index位置数组
  * @param {number} index 开始查找的坐标
  * @returns {number[]}
  */
- function findTargetInStrNext(str: string, target: string, limitNum: false | number, list: number[] = [], index = 0): number[] {
+ function findTargetInStrNext(str: string, target: string, limitNum: number, list: number[] = [], index = 0): number[] {
   const data = str.indexOf(target, index)
   if (data > -1) {
     list.push(data)
-    if (limitNum === false || limitNum > list.length) {
+    if (limitNum === 0 || limitNum > list.length) {
       list = findTargetInStrNext(str, target, limitNum, list, data + target.length)
     }
   }
@@ -22,7 +22,7 @@ import exportSelfMsg from './../utils/exportSelfMsg'
 
 type optionType = {
   limitNum?: number,
-  case?: true
+  case?: boolean
 }
 
 /**
@@ -31,14 +31,14 @@ type optionType = {
  * @param {string} target 需要查找的字符串
  * @param {object} [option] 设置项
  * @param {boolean} option.case 是否忽略大小写,默认不忽略
- * @param {false | number} option.limitNum 限制数量,false不限制
+ * @param {number} option.limitNum 限制数量,0不限制
  * @returns {number[]}
  */
 function findTargetInStr(str: string, target: string, option: optionType = {}): number[] {
   if (str && target) {
     str = str.toString()
     target = target.toString()
-    const limitNum = option.limitNum || false
+    const limitNum = option.limitNum || 0
     if (option.case) {
       str = str.toUpperCase()
       target = target.toUpperCase()
