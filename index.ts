098c96232a865fe4376e614750a1eb84a2df48f7
Shen GuoQing <yinweiyingzhe@126.com>
2023-02-24 14:46:19 +0800

    准备开始重构

--- index.ts
+++
@@ -1,7 +0,0 @@
-import $func from './src/main'
-import setData from './src/option/setData'
-import noticeData from './src/option/noticeData'
-
-export { noticeData, setData }
-
-export default $func
--- src/option/setData.ts
+++
@@ -1,9 +0,0 @@
-import { objectAny } from '../../ts'
-
-const setData = {
-  set: function(target: objectAny, prop: string, data: any) {
-    target[prop] = data
-  }
-}
-
-export default setData
